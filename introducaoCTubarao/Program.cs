99064fbc3dfc230efac5df41fa9335a44188bc3d
elis-ctrl <elis_emb@hotmail.com>
2026-08-17 22:54:22 -0300

    inclusão dos exercicios

--- introducaoCTubarao/Program.cs
+++ introducaoCTubarao/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//#01.Soma de dois números
+//#02.Antecessor e sucessor
+//#03.Area de um retangulo
+//#04.Conversor de moedas
+//#05.Calculadora Simples
+//#06.Contagem crescente
+//#07.Contagem regressiva
+//#08.Tabuada
+//#09.Soma dos números pares
+//#10.Login simples
+//#11.Soma até digitar zero
+//#12.Contador de caracteres
+//#13.Verificador de senha
+//#14.Maior elemento
+//#15.Média de um vetor
+//#16.Contagem de pares
+//#17.Ordem inversa
+//#18.Jogo da senha
+//#19.Caixa eletrônico
+
